21d3df6ad63ef0b54cdfa2e5e9c37c415225449a
abuanwar072 <abuanwar072@gmail.com>
2020-03-27 08:43:21 +0600

    payment add to admin

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Payment
 # Register your models here.
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'payment_type']
+    list_filter = ['payment_type', ]
+    search_fields = ['user']
+    list_per_page = 50
+
+
+admin.site.register(Payment, PaymentAdmin)
